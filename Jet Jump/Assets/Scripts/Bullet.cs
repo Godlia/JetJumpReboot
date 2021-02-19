d7bdd5b9c534d1cc2ef9afc13aeba7eed179897b
Godlia <Eirik.h.kristoffersen@gmail.com>
2021-02-19 09:43:08 +0100

    Update Bullet.cs

--- Jet Jump/Assets/Scripts/Bullet.cs
+++ Jet Jump/Assets/Scripts/Bullet.cs
@@ -7,7 +7,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -18,11 +18,5 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            Destroy(Bullet);
-        }
-
-        if (collision)
     }
 }
